1767a935f454f0f11b0c43fe90e4846032675b6e
taein <peble_@naver.com>
2024-11-12 13:13:48 +0900

    노마드코더 Authentication 챌린지 커밋2

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,13 +2,15 @@ import db from "@/lib/db";
 import getSession from "@/lib/session";
 import { notFound, redirect } from "next/navigation";
 
-async function getUser(){
+async function getUserData(){
   const { id } = await getSession();
 
   if(id){
     const user = await db.user.findUnique({ where: { id } });
+    const tweets = await db.tweet.findMany({ where: { userId: id } });
+    const likes = await db.like.findMany({ where: { userId: id }});
 
-    if(user) return user;
+    if(user) return { userInfo: user, tweets, likes };
   }
 
   notFound();
@@ -16,7 +18,7 @@ async function getUser(){
 
 
 export default async function Profile(){
-  const user = await getUser();
+  const { userInfo, tweets, likes } = await getUserData();
   const logout = async () => {
     "use server";
 
@@ -26,15 +28,12 @@ export default async function Profile(){
     redirect('/');
   }
 
-  console.log(user);
-
-
   return (
     <div>
-      <h1>{`반갑습니다 ${user.username}`}</h1>
-      <div>
-        {/* <span>{`당신의 트윗 수는 : ${user.tweets}`}</span> */}
-        <span></span>
+      <h1>{`반갑습니다 ${userInfo.username}`}</h1>
+      <div className="flex gap-3">
+        <span>{`당신의 트윗 수는 : ${tweets.length}`}</span>
+        <span>{`당신의 좋아요 수는 : ${likes.length}`}</span>
       </div>
       <form action={logout}>
         <button>Log out</button>
